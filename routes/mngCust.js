18f6001efad9fdc3de64a649b316228924235c29
Andrew Jung <jungan@oregonstate.edu>
2020-06-18 14:55:05 -0700

    routes

--- routes/mngCust.js
+++ routes/mngCust.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// test route to see if it works
 router.get('/manager', (req, res) => {
   res.send('manage customers page');
 });
+
+// PUT route for updating the customer list (includes marking custers as "entered")
+router.put('/');
 
 module.exports = router;
