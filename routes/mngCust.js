3fde0a64782558f16c13f3e83ead27bc48115d25
Andrew Jung <jungan@oregonstate.edu>
2020-06-18 15:13:23 -0700

    put route for marking customer as entered

--- routes/mngCust.js
+++ routes/mngCust.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Queue = require('../models/Queue');
 
 // test route to see if it works
 router.get('/manager', (req, res) => {
@@ -8,8 +9,13 @@ router.get('/manager', (req, res) => {
 
 // PUT route for updating the customer list (includes marking custers as "entered")
 router.put('/', async (req, res) => {
+  const { businessId, customerId } = req.body;
   try {
-  } catch (error) {}
+    const queue = await Queue.findOne({ business: businessId }); // find a matching queue with the businessId
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ msg: 'Error: no matching customer found' });
+  }
 });
 
 module.exports = router;
