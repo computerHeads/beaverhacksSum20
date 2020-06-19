25755aa32396c041efbce337169534812e665224
Andrew Jung <jungan@oregonstate.edu>
2020-06-18 17:44:58 -0700

    handlebars added

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const connectDb = require('./config/dB');
 
 const app = express();
 const PORT = process.env.PORT || 3000; // set port
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 app.use(express.json({ extended: false }));
+
 connectDb(); //  connect database
 
-app.get('/', (req, res) => res.send('App is working!')); // test route for server
+app.get('/', (req, res) => res.render('home')); // default route to app
 
 // define routes
 app.use('/manageCustomers', require('./routes/mngCust'));
