da77d3b2aaea459a8ff7193a6b5cee639a3dfc9b
Andrew Jung <jungan@oregonstate.edu>
2020-06-18 18:58:11 -0700

    + views and routes

--- routes/addCustomer.js
+++ routes/addCustomer.js
@@ -3,13 +3,24 @@ const router = express.Router();
 const Queue = require('../models/Queue');
 const Business = require('../models/Business');
 
-router.get('/:business_id', async (req, res) => {
+// route : /?id = :business_id
+router.get('/add', async (req, res) => {
   try {
-    let business = await Business.findOne({ business: req.business.id });
+    var payload = {};
+    let business = await Business.findOne({ business: req.query.id });
     if (!business) {
       return res.status(400).json({ msg: 'Error: could not find business' });
     }
-    res.json(business);
+    let queue = await Business.findOne({ business: req.query.id });
+    // first use and queue does not exist yet
+    if (!queue) {
+      payload.business = req.query.id;
+      queue = new Queue(payload);
+      await queue.save();
+    }
+    payload.business = business;
+    payload.queue = queue; // change this, don't need to send entire queue
+    res.render('addCustomer', payload);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server error');
