1da194cb1c101c277ab16d5185043d0206f72646
james <james.wei.moseley@gmail.com>
2020-06-18 22:27:24 -0700

    managerpage added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const connectDb = require('./config/dB');
+// const connectDb = require('./config/dB');
 
 const app = express();
 const PORT = process.env.PORT || 3000; // set port
@@ -8,7 +8,7 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.json({ extended: false }));
 
-connectDb(); //  connect database
+//connectDb(); //  connect database
 
 app.get('/', (req, res) => res.render('home')); // default route to app
 
