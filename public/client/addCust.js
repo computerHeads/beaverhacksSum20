d2713ed9cb156e21717fabe56f989c6da69a7b74
Andrew Jung <jungan@oregonstate.edu>
2020-06-20 09:27:36 -0700

    added country codes

--- public/client/addCust.js
+++ public/client/addCust.js
@@ -1,8 +1,13 @@
 const baseURL = 'http://localhost:3000/addCustomer/:business_id';
 
+$('#phone').intlTelInput();
+
 function sendForm() {
   var req = new XMLHttpRequest();
+
   var form = document.getElementsByTagName('form')[0];
+  var iti = intlTelInput(form.elements.phone);
+  var pnumber = iti.getNumber();
   var editedPhone = form.elements.phone.value,
     editedPhone = `+1${editedPhone.substring(0, 3)}${editedPhone.substring(
       4,
@@ -14,6 +19,7 @@ function sendForm() {
     email: form.elements.email.value,
     businessId: form.elements.id.value,
   };
+  console.log(pnumber);
   req.open('POST', baseURL, true);
   req.setRequestHeader('Content-Type', 'application/json');
   req.send(JSON.stringify(payload));
@@ -24,6 +30,7 @@ function sendForm() {
     submitBtn.setAttribute('onclick', `editCustomer('${response.customerId}')`);
 
     var cancel = document.createElement('button');
+    cancel.id = 'cancelBtn';
     cancel.innerHTML = 'Cancel reservation';
     cancel.setAttribute('onclick', `cancel('${response.customerId}')`);
     document.getElementById('cancel').appendChild(cancel); // add to document
@@ -53,17 +60,23 @@ function editCustomer(customerId) {
 
 // delete/cancel a reservation
 function cancel(customerId) {
-  document.getElementsByTagName('form')[0].reset();
   var req = new XMLHttpRequest();
+  const form = document.getElementsByTagName('form')[0];
   payload = {};
+  payload.name = form.elements.name.value;
+  payload.phone = form.elements.phone.value;
+  payload.email = form.elements.email.value;
   payload.businessId = document.getElementById('id').value;
   payload.customerId = customerId;
   req.open('DELETE', baseURL, true);
   req.setRequestHeader('Content-Type', 'application/json');
   req.send(JSON.stringify(payload));
   req.addEventListener('load', () => {
+    document.getElementsByTagName('form')[0].reset();
     document.getElementById('status').innerHTML =
       'Your reservation has been canceled';
+    var cancelBtn = document.getElementById('cancelBtn');
+    cancelBtn.parentNode.removeChild(cancelBtn); // remove the btn
   });
   event.preventDefault();
 }
